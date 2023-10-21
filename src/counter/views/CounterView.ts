ef2bf201e7657e1a9f93a09ac5610dd6eeae597b
Enrique Beltran <cesar78200921@hotmail.com>
2023-10-20 23:40:09 -0400

    - Cambios de codigo.

---
+++ src/counter/views/CounterView.ts
@@ -0,0 +1,18 @@
+﻿import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+  setup()
+  {
+
+    const counter = ref(5)
+
+    const increase = () => {
+      counter.value++;
+    }
+
+    return{
+      counter,
+      increase,
+    }
+  }
+});
