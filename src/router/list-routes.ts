3a7527f0d565b31464a61fc1a89c0bf0c2518fe2
Enrique Beltran <cesar78200921@hotmail.com>
2024-06-22 16:34:52 -0400

    - add route.

--- src/router/list-routes.ts
+++ src/router/list-routes.ts
@@ -9,6 +9,6 @@
 export const routerLinks: RouterLink[] = [
     {path: '/', name: 'home', title: 'Home', visible: true},
     {path: '/about', name: 'about', title: 'About', visible: true},
-    {path: '/counter', name: 'counter', title: 'Counter', visible: true},
+    {path: '/privacyspacesurfers', name: 'PrivacySpaceSurfers', title: 'PrivacySpaceSurfers', visible: true},
     {path: '/pokemons', name: 'pokemons', title: 'Pokemons', visible: true},
 ]
