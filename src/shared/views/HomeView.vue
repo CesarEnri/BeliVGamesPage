0737e9b5d89b29104712b154348075db1c4177dd
Enrique Beltran <cesar78200921@hotmail.com>
2024-06-22 16:27:31 -0400

    - Bug fix it.

--- src/shared/views/HomeView.vue
+++ src/shared/views/HomeView.vue
@@ -3,12 +3,6 @@
 
 <template>
   <main>
-    <h1>Building Web Site</h1>
-
-<img src="" />
-
-  <div class="image-container">
-<img src="src/assets/BeliVGamesWebPageDesignWeb.webp" alt="Imagen de ejemplo">
-</div>
+  
   </main>
 </template>
