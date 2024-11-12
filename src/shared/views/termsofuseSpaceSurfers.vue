8b686d005c886cfb4afb53bad9903fbf220366d6
Enrique Beltran <cesar78200921@hotmail.com>
2024-11-12 14:45:16 -0400

    - terms of use space surfers.

---
+++ src/shared/views/termsofuseSpaceSurfers.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+
+    <h1 style="color: black;">Terms of use Space Surfers Adventure</h1>
+      <p style="color: black;">
+        Terms of Use 
+
+        Welcome to  Space Surfers Adventure
+
+These terms and conditions describe the rules and regulations for using  Space Surfers Adventure.
+
+General Information
+This agreement applies to the terms and conditions, privacy notice, and any additional agreements. By accessing or using  Space Surfers Adventure, you agree to comply with these terms and conditions.
+
+Changes to the Agreement
+We reserve the right to modify this agreement at any time. We will notify you of such changes through any reasonable method, including the website or the app. Continued use of the game after changes have been made implies acceptance of the revised terms.
+
+Rules of Conduct
+To use  Space Surfers Adventure, you agree to comply with the following rules:
+
+You must not use the game for illegal or unauthorized activities.
+You must not interfere with the game’s operation.
+You must not reproduce, modify, or distribute the game’s content without our prior permission.
+Third-Party Links and Materials
+The game may include links to third-party resources. We do not control or endorse these materials and are not responsible for their content.
+
+Disclaimer of Warranties
+The game is provided "as is" without any warranty of any kind, express or implied, including warranties of merchantability, fitness for a particular purpose, or non-infringement.
+
+Contact
+If you have any questions or complaints about Space Surfers Adventure, contact us at contactbelivgames@gmail.com.
+      </p>
+
+  </main>
+</template>
+  
